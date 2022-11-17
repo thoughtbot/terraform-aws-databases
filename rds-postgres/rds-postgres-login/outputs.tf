0f61e2c414db3b3e51ac15de0f8e56bac6522f7a
Akshith Yellapragada <ayellapragada@gmail.com>
2022-11-17 11:04:42 -0500

    Match admin outputs

--- rds-postgres/rds-postgres-login/outputs.tf
+++ rds-postgres/rds-postgres-login/outputs.tf
@@ -1,6 +1,21 @@
+output "kms_key_arn" {
+  description = "ID of the KMS key used to encrypt the secret"
+  value       = module.secret.kms_key_arn
+}
+
 output "policy_json" {
   description = "Required IAM policies"
   value       = module.secret.policy_json
+}
+
+output "rotation_role_arn" {
+  description = "ARN of the IAM role allowed to rotate this secret"
+  value       = module.secret.rotation_role_arn
+}
+
+output "rotation_role_name" {
+  description = "Name of the IAM role allowed to rotate this secret"
+  value       = module.secret.rotation_role_name
 }
 
 output "secret_arn" {
