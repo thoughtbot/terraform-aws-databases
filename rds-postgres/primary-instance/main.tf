6960ce7f941c24e1e7e29afd88affb6c8cfbaeb3
Olamide <Olamide@thoughtbot.com>
2024-08-08 07:58:02 +0100

    Make KMS encryption optional for RDS and Elasticache

--- rds-postgres/primary-instance/main.tf
+++ rds-postgres/primary-instance/main.tf
@@ -156,5 +156,5 @@ locals {
     local.shared_vpc_security_group_ids
   )
 
-  primary_kms_key = var.kms_key_id == null ? module.customer_kms.kms_key_arn : var.kms_key_id
+  primary_kms_key = var.enable_kms ? (var.kms_key_id == null ? module.customer_kms.kms_key_arn : var.kms_key_id) : var.kms_key_id
 }
